abb7a860a3f70b0a1d4fbf12e14582e92b7b1699
mathieu royer <mathieu.c.royer@gmail.com>
2021-09-18 16:25:19 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "tasks", to: "tasks#index"
+  get "task/:id", to: "tasks#info", as: :task
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  delete "tasks/:id", to: "tasks#destroy", as: :delete
 end
--- db/migrate/20210918093502_add_new_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddNewTasks < ActiveRecord::Migration[6.0]
-  def change
-    add_column :title, :details, :completed
-  end
-end
